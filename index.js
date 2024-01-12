051e64e77dd5f5e0c96b508deb8d9e936302db97
scanunicco <Scanuproductions@gmail.com>
2024-01-12 10:26:37 -0500

    Add lighthouse

--- index.js
+++ index.js
@@ -339,18 +339,14 @@ function findMatchingRideEvents(query, arr) {
 //Stack overflow code for coordinates
 function getDistance(lat1, lon1, lat2, lon2) {
   var R = 3958.8; // Radius of the earth in miles
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLon = deg2rad(lon2-lon1);
-  var a =
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ;
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-  var d = R * c; // Distance in miles
+  var d = R * Math.acos(Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(lon1 - lon2)));
   return d;
 }
 
 function deg2rad(deg) {
-  return deg * (Math.PI/180)
+  return deg * (Math.PI/180);
+}
+
+function rad2deg(rad) {
+	return (rad * 180)/Math.PI;
 }
